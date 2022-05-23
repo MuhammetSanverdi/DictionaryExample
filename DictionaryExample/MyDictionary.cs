19d2b425efb2930dc936dd40ff1e9578a8e1acd1
MuhammetSanverdi <muhammetsanverdi97@gmail.com>
2022-05-23 23:45:39 +0300

    Add project files.

---
+++ DictionaryExample/MyDictionary.cs
@@ -0,0 +1,40 @@
+﻿namespace DictionaryExample
+{
+    class MyDictionary<T, X>
+    {
+        int[] _keys;
+        string[] _values;
+        public MyDictionary()
+        {
+            _keys = new int[0];
+            _values = new string[0];
+        }
+        public void Add(int key, string value)
+        {
+            int[] _tempkeys = _keys;
+            string[] _tempvalues = _values;
+            _keys = new int[_keys.Length + 1];
+            _values = new string[_values.Length+1];            
+            for (int i = 0; i < _tempkeys.Length; i++)
+            {
+                _keys[i] = _tempkeys[i];
+            }
+            for (int i = 0; i < _tempvalues.Length; i++)
+            {
+                _values[i] = _tempvalues[i];
+            }
+            _keys[_keys.Length-1] = key;
+            _values[_keys.Length-1] = value;
+        }
+
+        public int[] Keys
+        {
+            get { return _keys; }
+        }
+        public string[] Values
+        {
+            get { return _values; }
+        }
+
+    }
+}
---
+++ DictionaryExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DictionaryExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int,string> myDictionary = new MyDictionary<int,string>();            
+            myDictionary.Add(1, "Ahmet");
+            myDictionary.Add(2, "Mehmet");
+            myDictionary.Add(3, "Sadık");
+            myDictionary.Add(4, "Emin");
+            for (int i = 0; i < myDictionary.Keys.Length; i++)
+            {
+                Console.WriteLine(myDictionary.Keys[i]);
+                Console.WriteLine(myDictionary.Values[i]);
+            }
+        }
+    }
+}
